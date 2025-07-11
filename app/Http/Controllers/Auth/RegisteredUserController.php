671a5e9f06c01a8f655f044ba318df832efe092d
virga02 <virganpramesh@gmail.com>
2025-07-11 13:25:49 +0700

    test

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -19,7 +19,7 @@ class RegisteredUserController extends Controller
      */
     public function create(): View
     {
-        return view('template.auth.register');
+        return view('auth.register');
     }
 
     /**
